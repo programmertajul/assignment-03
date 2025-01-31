adb981bbe098d3b0e0e87bf72e3b5d668777388b
tajul <tajulislamaccabd@gmail.com>
2025-01-31 15:26:26 +0600

    header banner is completed for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      fontfamily: {
+
+        'adlam': ['ADLaM Display'],
+        'sora':['Sora'],
+
+      },
     },
     plugins: [],
   }
